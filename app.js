ddc0b846d7470078e99dd82487931e97b004343f
Rafi Ronquillo <rafironquillo@gmail.com>
2016-04-12 15:04:20 -0700

    Added final questions and a basic header on the page

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-// alert('Hey, let\'s play a guessing game about me!');
-//
-// alert('Before we get started, make sure to note that your answers must be either \'yes\' or \'no\'.');
+alert('Hey, let\'s play a guessing game about me!');
 
-// var answer1 = prompt('Does Rafi have a bike?').toUpperCase();
-//
-// if(answer1 === 'Y' || answer1 === 'YES') {
-//   alert('OHHH YEAHH');
-//   console.log('Does Rafi have a bike:' + answer1.toUpperCase());
-// }
-// else if(answer1 === 'N' || answer1 === 'NO'){
-//   alert('WRONG');
-//   console.log('So, does Rafi have a bike?:' + answer1.toUpperCase());
-// }
-// else{
-//   alert('sorry, try a yes/no answer!');
-// }
+alert('Before we get started, make sure to note that your answers must be either \'yes\' or \'no\'.');
+
+var answer1 = prompt('Does Rafi have a bike?').toUpperCase();
+
+if(answer1 === 'Y' || answer1 === 'YES') {
+  alert('OHHH YEAHH');
+  console.log('Does Rafi have a bike:' + answer1.toUpperCase());
+} else if(answer1 === 'N' || answer1 === 'NO'){
+  alert('WRONG');
+  console.log('So, does Rafi have a bike?:' + answer1.toUpperCase());
+} else{
+  alert('sorry, try a yes/no answer!');
+}
 
 var answer2 = prompt('Will Rafi be an expert coder?').toUpperCase();
 
@@ -43,3 +41,32 @@ else if( answer3 === 'N' || answer3 === 'NO'){
 else{
   alert('try again!');
 }
+
+var answer4 = prompt('Does Rafi live in Seattle?').toUpperCase();
+
+if(answer4 === 'Y' || answer4 === 'YES'){
+  alert('You\'re right, how did you know?');
+  console.log('Did they know you live in Seattle? ' + answer4);
+}
+else if( answer4 === 'N' || answer4 === 'NO'){
+  alert('Of course Rafi lives in Seattle!');
+  console.log('Do they know where you live?' + answer3);
+}
+else{
+  alert('try again!');
+}
+
+var answer5 = prompt('Does Rafi have a cat?').toUpperCase();
+
+if(answer5 === 'Y' || answer5 === 'YES'){
+  alert('He doesn\'t have one yet, but maybe soon.');
+  console.log('Does he have a cat? ' + answer4);
+}
+else if( answer5 === 'N' || answer5 === 'NO'){
+  alert('You\'re right, there\'s not a cat to be found!');
+  console.log('No cat?' + answer3);
+} else {
+  alert('try again!');
+}
+
+alert('Thanks for playing!');
