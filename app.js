c178a876566de6be078d08fd0709f713672b0f09
Rafi Ronquillo <rafironquillo@gmail.com>
2016-04-13 23:07:31 -0700

    finished question 7 and edited some minor Javascript

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ alert('Hey, let\'s play a guessing game about me!');
 
 alert('Before we get started, make sure to note that your answers must be either \'yes\' or \'no\'.');
 
+var totalCorrect = 0;
+var totalWrong = 0;
 var answer1 = prompt('Does Rafi have a bike?').toUpperCase();
 
 if(answer1 === 'Y' || answer1 === 'YES') {
   alert('You bet, he rides all the time!');
+  totalCorrect++;
   console.log('Does Rafi have a bike:' + answer1.toUpperCase());
 } else if(answer1 === 'N' || answer1 === 'NO'){
   alert('He actually does!');
+  totalWrong++;
   console.log('So, does Rafi have a bike?:' + answer1.toUpperCase());
 } else{
   alert('sorry, try a yes/no answer!');
@@ -18,6 +22,7 @@ var answer2 = prompt('Will Rafi be an expert coder?').toUpperCase();
 
 if(answer2 === 'Y' || answer2 === 'YES'){
   alert('Yes! You are an expert at predicting the future!');
+  totalCorrect++;
   console.log('Is this person great at predicting the future:' + answer2.toUpperCase());
 }
 else if(answer2 === 'N' || answer2 === 'NO'){
@@ -32,6 +37,7 @@ var answer3 = prompt('Does slow and steady win the race?').toUpperCase();
 
 if(answer3 === 'Y' || answer3 === 'YES'){
   alert('That is a very wise perspective');
+  totalCorrect++;
   console.log('Slow and steady works?: ' + answer3);
 }
 else if( answer3 === 'N' || answer3 === 'NO'){
@@ -46,6 +52,7 @@ var answer4 = prompt('Does Rafi live in Seattle?').toUpperCase();
 
 if(answer4 === 'Y' || answer4 === 'YES'){
   alert('You\'re right, how did you know?');
+  totalCorrect++;
   console.log('Did they know you live in Seattle? ' + answer4);
 }
 else if( answer4 === 'N' || answer4 === 'NO'){
@@ -60,6 +67,7 @@ var answer5 = prompt('Does Rafi have a cat?').toUpperCase();
 
 if(answer5 === 'Y' || answer5 === 'YES'){
   alert('He doesn\'t have one yet, but maybe soon.');
+  totalCorrect++;
   console.log('Does he have a cat? ' + answer4);
 }
 else if( answer5 === 'N' || answer5 === 'NO'){
@@ -82,6 +90,7 @@ while(guessTry < 4){
   }
   else if(question6 === 24){
     alert('You\'re right! congrats!');
+    totalCorrect++;
     break;
   }
   else if(question6 > 24){
@@ -94,4 +103,38 @@ while(guessTry < 4){
   guessTry++;
 }
 
-alert('Thanks for playing!');
+alert('Now on to the next round! Can you guess which pizza toppings I like? You have 6 tries to guess right, and you can guess multiple toppings at once, by separating them with commas!');
+var guessNumber = 0;
+var pizzaToppings = ['mushrooms', 'sausage','cheese','pepperoni'];
+
+while(guessNumber < 6){
+  var pizzaQuestion = prompt('What would I include on my dream pizza?');
+  if(pizzaQuestion === pizzaToppings[0]){
+    alert('You\'re right! I do love those toppings');
+    totalCorrect++;
+    break;
+  }
+  else if(pizzaQuestion === pizzaToppings[1]){
+    alert('You\'re right! I do love those toppings');
+    totalCorrect++;
+    break;
+  }
+  else if(pizzaQuestion === pizzaToppings[2]){
+    alert('You\'re right! I do love those toppings');
+    totalCorrect++;
+    break;
+  }
+  else if(pizzaQuestion === pizzaToppings[3]){
+    alert('You\'re right! I do love those toppings');
+    totalCorrect++;
+    break;
+  }
+  else{
+    alert('Woops, try another topping!');
+    guessNumber++;
+    totalWrong++;
+  }
+
+}
+
+alert('Thanks for playing! You got ' + totalCorrect + ' right and ' + totalWrong + ' wrong.');
